69753dd44ceb983c1dc4e2aae65f15c1a68201e0
Ibrahim Sabry <Ibrahim.Sabry@mail.com>
2019-09-26 15:07:12 +0200

    Injecting Market module in App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { MarketModule } from './market/market.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    MarketModule
     ],
   bootstrap: [AppComponent]
 })
